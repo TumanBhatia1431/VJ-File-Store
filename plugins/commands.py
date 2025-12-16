469b3a820edf8d3498e332b78ad38f5092152d8c
TumanBhatia1431 <157300467+TumanBhatia1431@users.noreply.github.com>
2025-12-16 06:25:12 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit@Tuman_Movies
+# Subscribe YouTube Channel For Amazing Bot @Tuman_Movies
+# Ask Doubt on telegram @Tuman_Movies
 
 import os
 import logging
@@ -24,9 +24,9 @@ logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
 
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit@Tuman_Movies
+# Subscribe YouTube Channel For Amazing Bot @Tuman_Movies
+# Ask Doubt on telegram @Tuman_Movies
 
 
 def get_size(size):
@@ -47,9 +47,9 @@ def formate_file_name(file_name):
     file_name = '@VJ_Botz ' + ' '.join(filter(lambda x: not x.startswith('http') and not x.startswith('@') and not x.startswith('www.'), file_name.split()))
     return file_name
 
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ0
+# Don't Remove Credit@Tuman_Movies
+# Subscribe YouTube Channel For Amazing Bot @Tuman_Movies
+# Ask Doubt on telegram @Tuman_Movies
 
 
 @Client.on_message(filters.command("start") & filters.incoming)
@@ -79,9 +79,9 @@ async def start(client, message):
         )
         return
 
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit@Tuman_Movies
+# Subscribe YouTube Channel For Amazing Bot @Tuman_Movies
+# Ask Doubt on telegram @Tuman_Movies
     
     data = message.command[1]
     try:
@@ -208,10 +208,9 @@ async def start(client, message):
             await k.edit_text("<b>Your All Files/Videos is successfully deleted!!!</b>")
         return
 
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+# Don't Remove Credit@Tuman_Movies
+# Subscribe YouTube Channel For Amazing Bot @Tuman_Movies
+# Ask Doubt on telegram @Tuman_Movies
     pre, decode_file_id = ((base64.urlsafe_b64decode(data + "=" * (-len(data) % 4))).decode("ascii")).split("_", 1)
     if not await check_verification(client, message.from_user.id) and VERIFY_MODE == True:
         btn = [[
@@ -267,9 +266,9 @@ async def start(client, message):
     except:
         pass
         
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit@Tuman_Movies
+# Subscribe YouTube Channel For Amazing Bot @Tuman_Movies
+# Ask Doubt on telegram @Tuman_Movies
 
 @Client.on_message(filters.command('api') & filters.private)
 async def shortener_api_handler(client, m: Message):
@@ -286,9 +285,9 @@ async def shortener_api_handler(client, m: Message):
         await update_user_info(user_id, {"shortener_api": api})
         await m.reply("<b>Shortener API updated successfully to</b> " + api)
 
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit@Tuman_Movies
+# Subscribe YouTube Channel For Amazing Bot @Tuman_Movies
+# Ask Doubt on telegram @Tuman_Movies
 
 @Client.on_message(filters.command("base_site") & filters.private)
 async def base_site_handler(client, m: Message):
@@ -309,9 +308,9 @@ async def base_site_handler(client, m: Message):
         await update_user_info(user_id, {"base_site": base_site})
         await m.reply("<b>Base Site updated successfully</b>")
 
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit@Tuman_Movies
+# Subscribe YouTube Channel For Amazing Bot @Tuman_Movies
+# Ask Doubt on telegram @Tuman_Movies
 
 @Client.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
@@ -335,9 +334,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
             parse_mode=enums.ParseMode.HTML
         )
 
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit@Tuman_Movies
+# Subscribe YouTube Channel For Amazing Bot @Tuman_Movies
+# Ask Doubt on telegram @Tuman_Movies
     
     elif query.data == "start":
         buttons = [[
@@ -364,9 +363,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
             parse_mode=enums.ParseMode.HTML
         )
 
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit@Tuman_Movies
+# Subscribe YouTube Channel For Amazing Bot @Tuman_Movies
+# Ask Doubt on telegram @Tuman_Movies
     
     elif query.data == "clone":
         buttons = [[
@@ -385,9 +384,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
             parse_mode=enums.ParseMode.HTML
         )          
 
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit@Tuman_Movies
+# Subscribe YouTube Channel For Amazing Bot @Tuman_Movies
+# Ask Doubt on telegram @Tuman_Movies
     
     elif query.data == "help":
         buttons = [[
@@ -406,6 +405,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             parse_mode=enums.ParseMode.HTML
         )  
         
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit@Tuman_Movies
+# Subscribe YouTube Channel For Amazing Bot @Tuman_Movies
+# Ask Doubt on telegram @Tuman_Movies
