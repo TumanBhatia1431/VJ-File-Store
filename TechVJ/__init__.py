2da64f8b9f625b6bd53a955e9e02ab3059da3b61
TumanBhatia1431 <157300467+TumanBhatia1431@users.noreply.github.com>
2025-12-16 06:04:11 +0530

    Update __init__.py

--- TechVJ/__init__.py
+++ TechVJ/__init__.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit @VJ_Bots
+# Don't Remove Credit @Tuman_Movies
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Ask Doubt on telegram @Tuman_Movies
 
 import time
 
